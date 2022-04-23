8755552d02db65dfde6a91682f0cc7a9faf4f5f5
JakubUherDev <jjuherdev@gmail.com>
2022-04-23 17:47:58 +0100

    Add: Initial home page

--- src/partials/Newsletter.jsx
+++ src/partials/Newsletter.jsx
@@ -39,18 +39,17 @@ function Newsletter() {
 
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
-                <h3 className="h3 text-white mb-2">Powering your business</h3>
-                <p className="text-gray-300 text-lg mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit nemo expedita voluptas culpa sapiente.</p>
+                <h3 className="h3 text-white mb-2">Chcesz rozwijac sie w <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-teal-300">HEMP</span> branzy?</h3>
+                <p className="text-gray-300 text-lg mb-6">Zostaw nam swojego maila, skontaktujemy sie z toba w przeciagu 7 dni.</p>
 
                 {/* CTA form */}
                 <form className="w-full lg:w-auto">
                   <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                    <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" />
-                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
+                    <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Twoj mail…" aria-label="Twoj mail…" />
+                    <a className="btn text-white bg-green-600 hover:bg-green-700 shadow" href="#0">Zostaw Maila</a>
                   </div>
                   {/* Success message */}
                   {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}
-                  <p className="text-sm text-gray-400 mt-3">7 days free trial. No credit card required.</p>
                 </form>
               </div>
 
--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -40,25 +40,24 @@ function Testimonials() {
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
 
 
-              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src={'src/images/eiha-logo.png'}/>
+              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src="../src/images/eiha-logo.png"/>
 
             </div>
 
             {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src={'src/images/gif-logo-bez-tla.png'}/>
+              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src="../src/images/gif-logo-bez-tla.png"/>
             </div>
 
             {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src={'src/images/lyme-logo.jpg'}/>
+              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src="../src/images/lyme-logo.jpg"/>
 
             </div>
 
             {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src={'src/images/shim-pol-logo-duze.png'}/>
-
+              <img className="max-w-full fill-current text-gray-400" width="124" height="24" src="../src/images/shim-pol-logo-duze.png"/>
             </div>
 
 
@@ -71,17 +70,17 @@ function Testimonials() {
               {/* Testimonial */}
               <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
                 <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
-                  <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-[#04926c]" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                     <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
                   </svg>
                   <img className="relative rounded-full" src={TestimonialImage} width="96" height="96" alt="Testimonial 01" />
                 </div>
-                <blockquote className="text-xl font-medium mb-4">
-                  “ I love this product and would recommend it to anyone. Could be not easier to use, and our multiple websites are wonderful. We get nice comments all the time. “
+                <blockquote className="text-xl text-gray-700 font-medium mb-4">
+                  “ Koncentrujemy sie na tworzeniu rozwiązań technologicznych w dziedzinie pozyskiwania i przetwarzania surowców roślinnych i wdrażanie tych rozwiązań na rynek. “
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Darya Finger</cite>
+                <cite className="block font-bold text-gray-900 text-lg not-italic mb-1">RAFAŁ MODLIŃSKI</cite>
                 <div className="text-gray-600">
-                  <span>CEO & Co-Founder</span> <a className="text-blue-600 hover:underline" href="#0">@Dropbox</a>
+                  <span>CBDO & Co-Founder</span> <a className="text-blue-600 hover:underline" href="#0">@CHT</a>
                 </div>
               </div>
 
